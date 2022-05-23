5eddefa69525c298fe538c4b16781533604097ac
Jarno Niklas Alanko <alanko.jarno@gmail.com>
2022-05-23 14:34:33 +0300

    Print warning if no BMI2 support

--- src/sbwt.cpp
+++ src/sbwt.cpp
@@ -19,6 +19,10 @@ void print_help(int argc, char** argv){
 
 int main(int argc, char** argv){
 
+    #ifndef __BMI2__
+    cerr << "WARNING: This program was compiled for a CPU without support for the BMI2 instruction set. The query performance of the Elias-Fano variants will be very bad" << endl;
+    #endif
+
     write_log("Maximum k-mer length is set to " + to_string(MAX_KMER_LENGTH), LogLevel::MAJOR);
 
     if(argc == 1){
