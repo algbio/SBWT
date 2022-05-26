72ab4a7d908ca8bd8b9c95f92b9e42bdb3d4a3b7
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2022-05-26 09:32:12 +0300

    make rc table const

--- src/globals.cpp
+++ src/globals.cpp
@@ -14,22 +14,22 @@ using namespace std::chrono;
 // Table mapping ascii values of characters to their reverse complements,
 // lower-case to lower case, upper-case to upper-case. Non-ACGT characters
 // are mapped to themselves.
-static unsigned char rc_table[256] = 
-{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
-20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 
-38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 
-56, 57, 58, 59, 60, 61, 62, 63, 64, 84, 66, 71, 68, 69, 70, 67, 72, 73, 
-74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 65, 85, 86, 87, 88, 89, 90, 91, 
-92, 93, 94, 95, 96, 116, 98, 103, 100, 101, 102, 99, 104, 105, 106, 107, 
-108, 109, 110, 111, 112, 113, 114, 115, 97, 117, 118, 119, 120, 121, 122, 
-123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 
-138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 
-153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 
-168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 
-183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 
-198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 
-213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 
-228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 
+static constexpr unsigned char rc_table[256] =
+{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
+38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
+56, 57, 58, 59, 60, 61, 62, 63, 64, 84, 66, 71, 68, 69, 70, 67, 72, 73,
+74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 65, 85, 86, 87, 88, 89, 90, 91,
+92, 93, 94, 95, 96, 116, 98, 103, 100, 101, 102, 99, 104, 105, 106, 107,
+108, 109, 110, 111, 112, 113, 114, 115, 97, 117, 118, 119, 120, 121, 122,
+123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137,
+138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152,
+153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167,
+168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182,
+183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197,
+198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212,
+213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227,
+228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242,
 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255};
 
 
@@ -46,11 +46,11 @@ string get_rc(const string& S){
 
 vector<string> readlines(string filename){
     vector<string> lines;
-	string line;
-	throwing_ifstream in(filename);
-	while(getline(in.stream,line)){
-		lines.push_back(line);
-	}
+    string line;
+    throwing_ifstream in(filename);
+    while(getline(in.stream,line)){
+        lines.push_back(line);
+    }
     return lines;
 }
 
@@ -86,18 +86,18 @@ string load_string(istream& in){
 }
 
 long long cur_time_millis(){
-	return (std::chrono::duration_cast< milliseconds >(high_resolution_clock::now().time_since_epoch())).count();
+    return (std::chrono::duration_cast< milliseconds >(high_resolution_clock::now().time_since_epoch())).count();
 }
 
 long long cur_time_micros(){
-	return (std::chrono::duration_cast< microseconds >(high_resolution_clock::now().time_since_epoch())).count();
+    return (std::chrono::duration_cast< microseconds >(high_resolution_clock::now().time_since_epoch())).count();
 }
 
 long long int program_start_millis = cur_time_millis();
 long long int program_start_micros = cur_time_micros();
 
 double seconds_since_program_start(){
-	return (cur_time_micros() - program_start_micros) / 1000.0;
+    return (cur_time_micros() - program_start_micros) / 1000.0;
 }
 
 string getTimeString(){
@@ -120,10 +120,10 @@ void write_log(string message, LogLevel level){
         std::lock_guard<std::mutex> lock(write_log_mutex);
         std::streamsize default_precision = std::cout.precision();
 
-        std::cerr << 
+        std::cerr <<
         std::setprecision(4) << std::fixed <<
         seconds_since_program_start() <<
-        std::setprecision(default_precision) << 
+        std::setprecision(default_precision) <<
         " " << getTimeString() << " " << message << std::endl;
     }
 }
@@ -143,8 +143,8 @@ vector<string> create_reverse_complement_files(const vector<string>& files){
         string f_rev = get_temp_file_manager().create_filename("", mode == SeqIO::FASTA ? ".fna" : ".fastq");
         newfiles.push_back(f_rev);
         SeqIO::Writer out(f_rev);
-        
-        while(true) { 
+
+        while(true) {
             LL len = sr.get_next_read_to_buffer();
             if(len == 0) break;
 
